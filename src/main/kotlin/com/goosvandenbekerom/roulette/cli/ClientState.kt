1d382b9b1ce2e321dbb5145eb98ec9f87054d924
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-24 14:15:02 +0200

    Changed individual queue from header exchange to topic exchange for performance

--- src/main/kotlin/com/goosvandenbekerom/roulette/cli/ClientState.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/cli/ClientState.kt
@@ -1,6 +1,11 @@
 package com.goosvandenbekerom.roulette.cli
 
 import com.goosvandenbekerom.roulette.core.Result
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.core.RabbitAdmin
+import org.springframework.beans.factory.annotation.Autowired
 
 class ClientState {
     var playerId: Long = -1
@@ -8,7 +13,13 @@ class ClientState {
     var bettingOpen = false
     val results = mutableSetOf<Result>()
 
+    @Autowired private lateinit var rabbit: RabbitAdmin
+    @Autowired private lateinit var exchange: TopicExchange
+    @Autowired private lateinit var queue: Queue
+
     fun connectPlayer(id: Long) {
         playerId = id
+        rabbit.declareBinding(BindingBuilder.bind(queue).to(exchange).with("player.$playerId"))
+        rabbit.initialize()
     }
 }
