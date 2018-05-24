659ed1cfd5ec0de84289b21513510c9981b894f5
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-24 14:32:07 +0200

    Added 2 unhandled message types

--- src/main/kotlin/com/goosvandenbekerom/roulette/cli/MessageHandler.kt
+++ src/main/kotlin/com/goosvandenbekerom/roulette/cli/MessageHandler.kt
@@ -1,11 +1,13 @@
 package com.goosvandenbekerom.roulette.cli
 
 import com.google.protobuf.Message
+import com.goosvandenbekerom.roulette.core.Player
 import com.goosvandenbekerom.roulette.core.Result
 import com.goosvandenbekerom.roulette.core.RouletteColor
 import com.goosvandenbekerom.roulette.proto.RouletteProto.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import kotlin.Error
 
 @Component
 class MessageHandler {
@@ -15,6 +17,8 @@ class MessageHandler {
         when(message) {
             is UpdateBettingStatus -> handleBettingStatusUpdate(message)
             is NewResult -> handleNewResult(message)
+            is PlayerAmountUpdate -> handlePlayerAmountUpdate(message)
+            is Error -> handleError(message)
             else -> println("Received currently unhandled message of type: ${message::class.simpleName}")
         }
     }
@@ -27,6 +31,15 @@ class MessageHandler {
     private fun handleNewResult(message: NewResult) {
         val result = Result(message.number, RouletteColor.valueOf(message.color))
         state.results.add(result)
-        println("received new result $result. amount of results: ${state.results.size}")
+        println("Received new result $result. amount of results: ${state.results.size}")
+    }
+
+    private fun handlePlayerAmountUpdate(message: PlayerAmountUpdate) {
+        state.chipAmount = message.amount
+        println("Received chips update. new amount: ${state.chipAmount}")
+    }
+
+    private fun handleError(error: Error) {
+        println("Error: ${error.message}")
     }
 }
